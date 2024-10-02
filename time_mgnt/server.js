39a5e5cb983ef75ef9d18500fff61cc4780be5b9
SHREYAS PADEKAR <102517554+yash-github012@users.noreply.github.com>
2024-10-02 11:30:27 +0530

    Add files via upload

---
+++ time_mgnt/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 5000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for the home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
